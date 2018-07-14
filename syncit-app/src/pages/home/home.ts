e9b8e4cb7b440e85cc0eb9d33607513e60615427
Santosh <santoshsai350@gmail.com>
2018-07-14 15:34:33 +0530

    Added multiple charts in single chart

--- syncit-app/src/pages/home/home.ts
+++ syncit-app/src/pages/home/home.ts
@@ -6,6 +6,7 @@ import { Chart } from 'chart.js';
 import Tesseract from 'tesseract.js'
 import visualRecognitionV3 from 'watson-developer-cloud/visual-recognition/v3'
 import { HttpService } from '../../services/http.service';
+import { transform } from 'lodash';
 
 @Component({
   selector: 'page-home',
@@ -98,17 +99,44 @@ export class HomePage {
     ionViewDidLoad() {
       this.httpService.getData('carbohydrates', '2018-07-01', '2018-07-11')
       .then((response) => {
-        let dataArray = response['data'];
+        let dataArray = this.transformResponse(response);//response['results'];
         let labelArray = response['labels'];
         let legend = response['legend'];
         this.options =  {
+              chart: {
+                type: 'line'
+              },
               title : { text :  legend},
-              series: [{
+              /* series: [{
                   type: 'area',
                   data: dataArray,
-              }]
+              }] */
+              series: dataArray,
+              
+            
           };;     
       });
+    }
+
+    transformResponse(responseData) {
+      return transform(responseData.results, (accumulator, value) => {
+        accumulator.push({'data': value.data, 'name': value.legend});
+      }, []);
+      /* {
+        data: [10, 40, 15, 46, 77.7, 20.4, 34.66, 22.4, 55.1, 83.7],
+        labels: ['2018-07-01', '2018-07-02', '2018-07-03', '2018-07-04', '2018-07-05',
+                '2018-07-06', '2018-07-07', '2018-07-08', '2018-07-09', '2018-07-10'            
+        ],
+        legend: 'Carbohydrate levels'
+    } */
+
+      /* [{
+          name: 'Jane',
+          data: [1, 0, 4]
+      }, {
+          name: 'John',
+          data: [5, 7, 3]
+      }] */
     }
     ionViewDidLoad1() {  
         this.httpService.getData('carbohydrates', '2018-07-01', '2018-07-11')
